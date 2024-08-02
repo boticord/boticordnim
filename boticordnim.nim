27e94642f6c9b4584df46437d6b94bb93a05e0da
bit0r1n <me@bitor.in>
2024-08-02 12:53:33 +0300

    docs: main page

--- boticordnim.nim
+++ boticordnim.nim
@@ -1,3 +1,18 @@
+## Welcome to boticordnim Documentaion!
+##
+## Reference
+## =========
+## * Resources modules
+##   - `bots` Includes methods to get data about specific bot or post statistics of your own bots
+##   - `servers` and `users` Includes methods to get data about specific server and user profile respectively
+##   - `search` Includes methods to perform search among server, bots and comments of previously mentioned resources
+## * Gateway
+##   - `notifications` Includes method to start receiving events from Boticord in real-time
+##
+## Examples
+## ========
+## Examples of usage are located in [project's readme](https://github.com/boticord/boticordnim) and [examples folder](https://github.com/boticord/boticordnim/tree/master/examples)
+
 import boticordnim/[bots, notifications, search, servers, typedefs, users]
 
 export bots, notifications, search, servers, typedefs, users
